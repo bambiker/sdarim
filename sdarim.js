db4c982a55f96d75472feded423b28e00d096040
bambiker <50655137+bambiker@users.noreply.github.com>
2024-05-14 17:06:35 +0300

    Update sdarim.js

--- sdarim.js
+++ sdarim.js
@@ -7,18 +7,7 @@ async function chapter2seder(){
     console.log(json);
     console.log(json.seder[3].bookseder);
 
-   tt =  {"list": [
-    {"name":"my Name","id":12,"type":"car owner"},
-    {"name":"my Name2","id":13,"type":"car owner2"},
-    {"name":"my Name4","id":14,"type":"car owner3"},
-    {"name":"my Name4","id":15,"type":"car owner5"}
-]}
-
-    console.log(tt); 
-    console.log(tt.list[1])
-    console.log(tt.list[1].name)
-    
-    payment = seder[1][parasha];
+    payment = json.seder[3].bookseder;
     pay2   = 'gg'; //seder[1].[verseseder];
     return payment;
 
