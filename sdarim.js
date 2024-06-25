493bbc9cf9616f523a17237a45dbe7097838c6c7
bambiker <50655137+bambiker@users.noreply.github.com>
2024-06-25 12:32:40 +0300

    Update sdarim.js

--- sdarim.js
+++ sdarim.js
@@ -929,7 +929,7 @@ function cleanverse(para){
   return paraclean;  
 }
 
-function userSeder(book, seder, verse){
+async function userSeder(book, seder, verse){
     //  seder = seder.trim()
     //  verse = verse.trim()
     if (verse=='') verse= 'א'
